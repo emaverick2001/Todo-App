b4a3ec4ac02f85de1c09c495c6a6a5857bb48ab1
Mav <mespin11@jh.edu>
2024-06-10 12:16:07 -0700

    test3

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import "../style.css";
 // Define the state of our app
 const todos = [
   { id: 1, text: "Buy milk", completed: false },
-  { id: 2, text: "Buy bread", completed: false },
-  { id: 3, text: "Buy coffee", completed: true },
+  { id: 2, text: "Buy bread", completed: true },
+  { id: 3, text: "Buy coffee", completed: false },
 ];
 let nextTodoId = 4;
 let filter = "all"; // can be 'all', 'active', or 'completed'
@@ -32,8 +32,21 @@ function renderTodos() {
     const todoList = document.getElementById("todo-list");
     todoList.innerHTML = ""
 
+    let filteredTodos = [];
     for(let i = 0; i < todos.length; i++) {
       const todo = todos[i];
+
+      if (filter === "all") {
+        filteredTodos.push(todo);
+      } else if (filter === "active" && !todo.completed) {
+        filteredTodos.push(todo);
+      } else if (filter === "completed" && todo.completed) {
+        filteredTodos.push(todo);
+      }
+    }
+
+    for(let i = 0; i < filteredTodos.length; i++) {
+      const todo = filteredTodos[i];
 
       const todoItem = document.createElement("div");
       todoItem.classList.add("p-4", "todo-item");
@@ -41,7 +54,12 @@ function renderTodos() {
 
       const todoText = document.createElement("div");
       todoText.classList.add("todo-text");
+      if (todo.completed) {
+        todoText.classList.add("line-through");
+      }
+      
       todoText.innerText = todo.text;
+      todoText.id = `todo-text-${todo.id}`;
       todoItem.appendChild(todoText)
 
       const todoEdit = document.createElement("input");
@@ -50,6 +68,8 @@ function renderTodos() {
       todoItem.appendChild(todoEdit)
     }
 }
+
+document.addEventListener("DOMContentLoaded", renderTodos)
 
 
 /*
@@ -118,13 +138,66 @@ function handleAddNewTodo(event) {
 
   */
 
-const navBar = document.getElementById("todo-nav")
-navBar.classList.add("p-6", "border-2", "border-red-500")
-navBar.addEventListener("click", handleClickOnNavbar)
+  const navBar = document.getElementById("todo-nav")
+  // navBar.classList.add("p-6", "border-2", "border-red-500")
+  navBar.addEventListener("click", handleClickOnNavbar)
 
-function handleClickOnNavbar(event) {
-console.log(event);
-}
+  function handleClickOnNavbar(event) {
+    // console.log(event.target.tagName);
+    if (event.target.tagName === "A") {
+      // console.log(event.target.href)
+      const href = event.target.href;
+      const action = href.split("/").pop();
+      filter = action === "" ? "all" : action
+      // console.log(filter)
+      renderTodos();
+      renderNavbar(href);
+    }
+  }
+
+  function renderNavbar(href) {
+    const elements = navBar.children;
+    for(let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      if (element.href === href) {
+        element.classList.add(
+          "underline",
+          "underline-offset-4", 
+          "decoration-rose-800", 
+          "decoration-2"
+        )
+      } else {
+        element.classList.remove(
+          "underline",
+          "underline-offset-4", 
+          "decoration-rose-800", 
+          "decoration-2"
+        )
+      }
+    }
+  }
 
 
-document.addEventListener("DOMContentLoaded", renderTodos)
+/*
+    <section
+      id="todo-list"
+      class="flex flex-col w-full text-xl font-normal divide-y"
+    >
+      <div class="p-4 todo-item">
+        <div class="todo-text">Buy milk</div>
+        <input class="hidden todo-edit" value="Buy milk" />
+      </div>
+      <div class="p-4 todo-item">
+        <div class="todo-text">Buy bread</div>
+        <input class="hidden todo-edit" value="Buy bread" />
+      </div>
+    </section>
+  */
+
+const todoList = document.getElementById("todo-list");
+todoList.classList.add("p-6", "border-2", "border-red-500")
+todoList.addEventListener("click", handleClickOnTodoList)
+
+// function handleClickOnTodoList(event) {
+//   // console.log(event);
+//   let todo = null;
